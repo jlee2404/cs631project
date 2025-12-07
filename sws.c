c0ea9dff8230e6f33be0ba0230ddbfc9c0fcabf2
Justin Lee <jlee16@stevens.edu>
2025-12-07 03:41:45 +0000

    implemented URI resolution and basic request flow

--- sws.c
+++ sws.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/wait.h>
 
@@ -8,6 +9,7 @@
 #include <errno.h>
 #include <limits.h>
 #include <netdb.h>
+#include <pwd.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +25,10 @@
 
 #ifndef SLEEP
 #define SLEEP 5
+#endif
+
+#ifndef MAXUSERNAME
+#define MAXUSERNAME 256 /* 255 is classic UNIX username limit */
 #endif
 
 void
@@ -63,10 +69,102 @@ createSocket(struct addrinfo *info)
     return sock;
 }
 
-void
-handleConnection(int fd, struct sockaddr_in6 client)
+int
+uriToPath(const char *docroot, const char *uri, char *outpath, 
+    size_t outsize, struct stat *statbuf, int *flags_out)
 {
-    int rv;
+    if (!docroot || !uri || !outpath || outsize == 0) {
+	return -1;
+    }
+
+    *flags_out = 0;
+
+    if (strstr(uri, "..") != NULL) {
+	errno = EACCES;
+        return -1;
+    }
+
+    if (uri[0] == '~') {
+	const char *ptr = uri + 1;
+	size_t uname_len = 0;
+	while (*ptr && *ptr != '/' && uname_len < MAXUSERNAME) {
+	    uname_len++;
+	    ptr++;
+	}
+
+	if (uname_len == 0 || uname_len >= MAXUSERNAME) {
+	    return -1;
+	}
+	
+	char uname[MAXUSERNAME + 1];
+	memcpy(uname, uri + 1, uname_len);
+	uname[uname_len] = '\0';
+
+	struct passwd *pw = getpwnam(uname);
+	if (!pw) {
+	    return -1;
+	}
+
+	const char *rest = uri + 1 + uname_len;
+	if (rest[0] == '\0') {
+	    if (snprintf(outpath, outsize, "%s/sws", pw->pw_dir) >= 
+		(int)outsize) {
+		return -1;
+	    }
+	} else {
+	    if (snprintf(outpath, outsize, "%s/sws%s", pw->pw_dir, rest) >= 
+		(int)outsize) {
+		return -1;
+	    }
+	}
+    } else {
+	if (uri[0] != '/') {
+	    return -1;
+	}
+
+	if (strcmp(uri, "/") == 0) {
+	    if (snprintf(outpath, outsize, "%s/", docroot) >= (int)outsize) {
+		return -1;
+	    }
+	} else {
+	    if (snprintf(outpath, outsize, "%s%s", docroot, uri) >= 
+		(int)outsize) {
+		return -1;
+	    }
+	}
+    }
+
+    if (stat(outpath, statbuf) == 0) {
+	*flags_out |= 1; /* resolved and exists */
+	if (S_ISDIR(statbuf->st_mode)) {
+	    *flags_out |= 2; /* is direcotry */
+	    size_t urilen = strlen(uri);
+	    if (urilen == 0 || uri[urilen-1] != '/') {
+		*flags_out |= 4; /* needs trailing slash redirect */
+	    } else {
+		char indexpath[PATH_MAX];
+		if (snprintf(indexpath, sizeof(indexpath), "%sindex.html", outpath) >= 
+		    (int)sizeof(indexpath)) {
+		    return -1;
+		}
+		if (stat(indexpath, statbuf) == 0) {
+		    if (snprintf(outpath, outsize, "%s", indexpath) >= (int)outsize) {
+			return -1;
+		    }
+		    *flags_out &= ~2; /* file now */
+		    *flags_out |= 1;
+		}
+	    }
+	}
+    }
+
+    return 0;
+}
+
+void
+handleConnection(int fd, struct sockaddr_in6 client, const char *dir)
+{
+    int rd;
     int total_read = 0;
     char buf[BUFSIZ];
     char claddr[INET6_ADDRSTRLEN];
@@ -76,7 +174,7 @@ handleConnection(int fd, struct sockaddr_in6 client)
 
     memset(&req, 0, sizeof(struct request));
     
-    if ((rv = read(fd, buf+total_read, BUFSIZ-total_read-1)) <= 0) {
+    if ((rd = read(fd, buf+total_read, BUFSIZ-total_read-1)) <= 0) {
         perror("reading stream message");
         goto exit;
     }
@@ -86,26 +184,47 @@ handleConnection(int fd, struct sockaddr_in6 client)
         rip = "unkown";
     }
     if (parseRequest(buf, &req) == 0) {
-        (void)printf("Client: %s\nMethod: %s\nURI: %s\nHeader: %s\n", rip, req.method, req.uri, req.header);
+	char fullpath[PATH_MAX];
+	struct stat sb;
+	int flags = 0;
 
-	response = "HTTP/1.0 200 OK\r\n"
+	int rv = uriToPath(dir, req.uri, fullpath, sizeof(fullpath), &sb, &flags);
+	if (rv < 0) {
+	    response = "HTTP/1.0 403 Forbidden\r\n"
+	        "Content-Type: text/plain\r\n"
+	    	"Content-Length: 10\r\n\r\n"
+	    	"Forbidden\r\n";
+	    goto exit;
+	}
+
+	if (!(flags & 1)) {
+	    response = "HTTP/1.0 404 Not Found\r\n"
+	    	"Content-Type: text/plain\r\n"
+	    	"Content-Length: 10\r\n\r\n"
+	    	"Not Found\r\n";
+	    goto exit;
+	}
+
+	if (flags & 4) {
+	    char location[PATH_MAX];
+	    snprintf(location, sizeof(location), "Location: %s/\r\n", req.uri);
+	    char header[256]; /* 256 is safely large enough without going overboard */
+	    snprintf(header, sizeof(header), "HTTP/1.0 301 Moved Permanently\r\n%s\r\n", location);
+	    response = header;
+	    goto exit;
+	}
+
+	char headerBuf[512]; /* 512 is safely large enough without going overboard */
+	snprintf(headerBuf, sizeof(headerBuf),
+	    "HTTP/1.0 200 OK\r\n"
 	    "Content-Type: text/plain\r\n"
 	    "Content-Length: 19\r\n\r\n"
-	    "Request valid: OK\r\n";
-    } else {
-	(void)printf("Client: %s\nParse failed\n", rip);
-
-	response = "HTTP/1.0 400 Bad Request\r\n"
-	    "Content-Type: text/plain\r\n"
-	    "Content-Length: 15\r\n\r\n"
-	    "Bad Request\r\n";
-
-	goto exit;
+	    "Request valid: OK\r\n");
+	response = headerBuf;
     }
 
-    write(fd, response, strlen(response));
-
 exit:
+    write(fd, response, strlen(response));
     if (close(fd) < 0) {
         perror("close");
         exit(EXIT_FAILURE);
@@ -115,7 +234,7 @@ exit:
 }
 
 void
-handleSocket(int sock)
+handleSocket(int sock, const char *dir)
 {
     int fd;
     pid_t pid;
@@ -134,7 +253,7 @@ handleSocket(int sock)
         perror("fork");
         exit(EXIT_FAILURE);
     } else if (pid == 0) { /* child */
-        handleConnection(fd, client);
+        handleConnection(fd, client, dir);
     }
 
     /* parent returns */
@@ -238,7 +357,7 @@ main(int argc, char **argv)
                 perror("select");
             }
         } else if (FD_ISSET(sock, &ready)) {
-            handleSocket(sock);
+            handleSocket(sock, dir);
         }
     }
 
--- sws.h
+++ sws.h
@@ -2,9 +2,9 @@
 #define _SWS_H_
 
 int main(int, char **);
-void handleConnection(int, struct sockaddr_in6);
+void handleConnection(int, struct sockaddr_in6, const char *);
 int createSocket(struct addrinfo *);
-void handleSocket(int);
+void handleSocket(int, const char *);
 void usage(void);
 
 #endif
